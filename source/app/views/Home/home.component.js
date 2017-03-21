86b60375fcc3337c48f602452aaebb99e685e8f8
Jorge Daniel Valverde <jvalverde@konradgroup.com>
2017-03-21 10:50:02 -0600

    Removed the lorems in Home

--- source/app/views/Home/home.component.js
+++ source/app/views/Home/home.component.js
@@ -10,16 +10,7 @@ module.exports = () =>
             <Image css='logo' source={react_icon} />
         </div>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam eligendi voluptatem, repellendus! Accusamus repudiandae quos maiores tenetur nostrum dolores voluptate sint non aliquid amet, ipsa esse officiis omnis vel, aliquam.
-        </p>
-        <p>
-            Hic esse sit nisi aperiam unde illo laborum numquam incidunt laudantium? Officia voluptatibus consequatur, ea doloribus nisi exercitationem, voluptatum esse veritatis vitae sit libero optio iusto nam mollitia. Est, eum!
-        </p>
-        <p>
-            Incidunt id illum impedit cumque praesentium debitis, corporis consequuntur porro dicta dignissimos dolor, at saepe harum autem sunt quae, reiciendis earum. Aliquam eos repellat voluptatibus modi illo quasi culpa, et.
-        </p>
-        <p>
-            Saepe quis recusandae, quo expedita nulla. Dolor, quae, eos numquam et harum ut quo, quod dolorem maxime totam voluptates molestias blanditiis incidunt aperiam. Eum officiis soluta aspernatur earum. Ex, tempora?
-        </p>        
+            Simple React-Redux boilerplate for a webapplication.
+        </p>    
     </div>
 ;
