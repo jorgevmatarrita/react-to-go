a8404a0f173ba24d9622cf0c49cd6a73b84fb7ec
Jorge Daniel Valverde <jvalverde@konradgroup.com>
2017-03-24 12:51:41 -0600

    webpack production config bug fixed

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -1,6 +1,7 @@
 const commonConfig = require('./webpack.common.config');
 
-module.exports = {
-    ...commonConfig,
-    watch: false
-};
+module.exports = Object.assign(commonConfig,
+    {
+        watch: false
+    }
+);
